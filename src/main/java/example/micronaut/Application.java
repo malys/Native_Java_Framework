011992cfc356df7bdff32820f7dec7dc77d52d4d
Sylvain Malnuit <sylvain.malnuit@lyra-network.com>
2019-09-05 15:26:47 +0200

    micronaut spring conversion

--- src/main/java/example/micronaut/Application.java
+++ src/main/java/example/micronaut/Application.java
@@ -1,13 +1,17 @@
 package example.micronaut;
 
+import io.micronaut.runtime.Micronaut;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //java -Dserver.port=8081 -jar
+// ./gradlew assemble
+// native-image --no-server -cp build/libs/complete-all.jar
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+         Micronaut.run(Application.class);
+        //SpringApplication.run(Application.class, args);
     }
 
 }
